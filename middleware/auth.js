bfdee93aa72774b6fc10c14c6ec8cbce269c900c
Aman <amanansari.a2@gmail.com>
2020-01-29 11:43:45 +0530

    add middleware

---
+++ middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+function auth(req, res, next) {
+  const token = req.header("x-auth-token");
+
+  //check for token
+  if (!token) res.status(401).json({ msg: "No token, authorization denied" });
+  try {
+    // verify token
+    const decoded = jwt.verify(token, "MyF*cking__Sec");
+    //Add user from payload
+    req.user = decoded;
+    next();
+  } catch (e) {
+    res.status(400).json({ msg: "Token is not valid" });
+  }
+}
+
+module.exports = auth;
