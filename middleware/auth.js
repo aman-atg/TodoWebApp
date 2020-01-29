2c036a03a1aa966eaa5bf15c70450b33198fc81c
Aman <amanansari.a2@gmail.com>
2020-01-29 20:38:38 +0530

    bug-fix

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,8 @@ function auth(req, res, next) {
   const token = req.header("x-auth-token");
 
   //check for token
-  if (!token) res.status(401).json({ msg: "No token, authorization denied" });
+  if (!token)
+    return res.status(401).json({ msg: "No token, authorization denied" });
   try {
     // verify token
     const decoded = jwt.verify(token, "MyF*cking__Sec");
