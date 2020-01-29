dd01440fde948f75851e97e4488f8deed2246645
Aman <amanansari.a2@gmail.com>
2020-01-30 02:21:37 +0530

    left

--- client/src/actions/authActions.jsx
+++ client/src/actions/authActions.jsx
@@ -28,8 +28,34 @@ export const loadUser = () => (dispatch, getState) => {
     });
 };
 
-// setup config/headers and token
+// Register User
+export const Register = ({ name, email, password }) => dispatch => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+  // Request body
+  const body = JSON.stringify({ name, email, password });
+  axios
+    .post("/api/users", body, config)
+    .then(res =>
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: res.data
+      })
+    )
+    .catch(err => {
+      dispatch(
+        returnErrors(err.response.data, err.response.status, "REGISTER_FAIL")
+      );
+      dispatch({
+        type: REGISTER_FAIL
+      });
+    });
+};
 
+// setup config/headers and token
 export const tokenConfig = getState => {
   // Get token from localStorage
   const { token } = getState().auth;
