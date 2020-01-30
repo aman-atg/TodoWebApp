5dc9023af6a78fa731e1d8dab91e6012a8a88e50
Aman <amanansari.a2@gmail.com>
2020-01-30 23:47:09 +0530

    error handling + tokeConfig

--- client/src/actions/itemActions.jsx
+++ client/src/actions/itemActions.jsx
@@ -1,32 +1,49 @@
 import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from "./types";
 import axios from "axios";
+import { tokenConfig } from "./authActions";
+import { returnErrors } from "./errorActions";
 
 export const getItems = () => dispatch => {
   dispatch(setItemsLoading());
-  axios.get("/api/items").then(res => {
-    dispatch({
-      type: GET_ITEMS,
-      payload: res.data
-    });
-  });
-};
-
-export const addItem = item => dispatch => {
-  axios.post("/api/items", item).then(res =>
-    dispatch({
-      type: ADD_ITEM,
-      payload: res.data
+  axios
+    .get("/api/items")
+    .then(res => {
+      dispatch({
+        type: GET_ITEMS,
+        payload: res.data
+      });
     })
-  );
+    .catch(err => {
+      dispatch(returnErrors(err.response.data, err.response.status));
+    });
 };
 
-export const deleteItem = id => dispatch => {
-  axios.delete(`/api/items/${id}`).then(res => {
-    dispatch({
-      type: DELETE_ITEM,
-      payload: id
+export const addItem = item => (dispatch, getState) => {
+  axios
+    .post("/api/items", item, tokenConfig(getState))
+    .then(res =>
+      dispatch({
+        type: ADD_ITEM,
+        payload: res.data
+      })
+    )
+    .catch(err => {
+      dispatch(returnErrors(err.response.data, err.response.status));
     });
-  });
+};
+
+export const deleteItem = id => (dispatch, getState) => {
+  axios
+    .delete(`/api/items/${id}`, tokenConfig(getState))
+    .then(res => {
+      dispatch({
+        type: DELETE_ITEM,
+        payload: id
+      });
+    })
+    .catch(err => {
+      dispatch(returnErrors(err.response.data, err.response.status));
+    });
 };
 
 export const setItemsLoading = () => {
