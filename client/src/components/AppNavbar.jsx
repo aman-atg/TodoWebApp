b826c13a6ee76144882f9fc10e8a4ce8f167cd6c
Aman <amanansari.a2@gmail.com>
2020-01-30 23:48:11 +0530

    loginModal + UI improvement

--- client/src/components/AppNavbar.jsx
+++ client/src/components/AppNavbar.jsx
@@ -9,18 +9,51 @@ import {
   NavLink,
   Container
 } from "reactstrap";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 import RegisterModal from "./auth/RegisterModal";
 import Logout from "./auth/Logout";
+import LoginModal from "./auth/LoginModal";
+import { Fragment } from "react";
 
 class AppNavbar extends Component {
   state = {
     isOpen: false
   };
 
+  static propTypes = {
+    auth: PropTypes.object.isRequired
+  };
+
   toggle = () => {
     this.setState({ isOpen: !this.state.isOpen });
   };
+
   render() {
+    const { isAuthenticated, user } = this.props.auth;
+    const authLinks = (
+      <Fragment>
+        <NavItem>
+          <span className="navbar-text mr-3">
+            <strong>{user ? `Welcome ${user.name}` : ""}</strong>
+          </span>
+        </NavItem>
+        <NavItem>
+          <Logout />
+        </NavItem>
+      </Fragment>
+    );
+    const guestLinks = (
+      <Fragment>
+        <NavItem>
+          <RegisterModal />
+        </NavItem>
+        <NavItem>
+          <LoginModal />
+        </NavItem>
+      </Fragment>
+    );
+
     return (
       <div className="">
         <Navbar className="mb-5" color="dark" dark expand="sm">
@@ -32,12 +65,7 @@ class AppNavbar extends Component {
                 <NavItem>
                   <NavLink href="https://github.com/aman-atg">Github</NavLink>
                 </NavItem>
-                <NavItem>
-                  <RegisterModal />
-                </NavItem>
-                <NavItem>
-                  <Logout />
-                </NavItem>
+                {isAuthenticated ? authLinks : guestLinks}
               </Nav>
             </Collapse>
           </Container>
@@ -47,4 +75,8 @@ class AppNavbar extends Component {
   }
 }
 
-export default AppNavbar;
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, null)(AppNavbar);
